bbb0eb2e99c116b2adc1fa50f96f84bca122d185
GuilhermeBertola1 <96829993+GuilhermeBertola1@users.noreply.github.com>
2023-06-30 21:35:43 -0300

    button

---
+++ ESP32/Test/testWEB/script.js
@@ -0,0 +1,36 @@
+const chk = document.getElementById('chk');
+const chk1 = document.getElementById('chk1');
+const chk2 = document.getElementById('chk2');
+const chk3 = document.getElementById('chk3');
+
+chk.addEventListener('change', () => {
+    if(chk.checked){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+});
+
+chk1.addEventListener('change', () => {
+    if(chk1.checked){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+});
+
+chk2.addEventListener('change', () => {
+    if(chk2.checked){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+});
+
+chk3.addEventListener('change', () => {
+    if(chk3.checked){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+});
