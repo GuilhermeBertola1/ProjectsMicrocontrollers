7e19f98ec83dc0ce9be4d41e6b3558cdc63125f9
GuilhermeBertola1 <96829993+GuilhermeBertola1@users.noreply.github.com>
2024-01-02 09:59:17 -0300

    Update main.cpp

--- MQTT/src/main.cpp
+++ MQTT/src/main.cpp
@@ -6,7 +6,7 @@ const char* rede = "BERTOLA_2.4G";
 const char* senha = "170704gui";
 
 //mqtt broker - conexao
-const char* mqtt_broker = "192.168.0.15";
+const char* mqtt_broker = "x";
 const char* topic = "topico/auto";
 const char* mqtt_user = "Bertola123";
 const char* mqtt_pas = "170704gui";
